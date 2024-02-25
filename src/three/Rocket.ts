71b2a44c8995a051de4871810a91374d67f915b8
Dipokal HHJ <hhj@devent.kr>
2024-02-25 15:00:54 +0900

    feat: add rocket model

--- src/three/Rocket.ts
+++ src/three/Rocket.ts
@@ -1,7 +1,8 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 class Rocket {
-  parent: THREE.Object3D<THREE.Object3DEventMap> | any;
+  parent!: THREE.Object3D<THREE.Object3DEventMap>;
   constructor() {
     this.addParent();
     this.addRocketModel();
@@ -12,10 +13,23 @@ class Rocket {
   }
 
   private addRocketModel() {
-    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    const material = new THREE.MeshStandardMaterial();
-    const mesh = new THREE.Mesh(geometry, material);
-    this.parent.add(mesh);
+    const loader = new GLTFLoader();
+
+    loader.load(
+      "/saturnv.glb",
+      (gltf) => {
+        const scale = 0.1;
+        gltf.scene.scale.set(scale, scale, scale);
+        gltf.scene.rotation.set(-Math.PI / 2, 0, 0);
+        this.parent.add(gltf.scene);
+      },
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      function (error) {
+        console.log("An error happened");
+      }
+    );
   }
 }
 
